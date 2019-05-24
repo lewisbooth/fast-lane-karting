055f5d2a9101cf717bea74f2c9dd20a6214737ea
Lewis <lewis@amp.studio>
2019-05-24 15:24:17 +0100

    Fixed bug calculating Saturday opening hours

--- src/js/openingHours.js
+++ src/js/openingHours.js
@@ -12,7 +12,7 @@ setTimeout(updateLight, 3600)
 function updateLight() {
   if (hour >= 10 && hour < 22)
     changeStatus('open-now')
-  if (day == 6 && hour >= 9 && hour < 22)
+  else if (day == 6 && hour >= 9 && hour < 22)
     changeStatus('open-now')
   else if (hour == 22 && minutes < 30)
     changeStatus('closing-soon')
@@ -21,6 +21,7 @@ function updateLight() {
 }
 
 function changeStatus(status) {
+  console.log(status)
   light.classList.add(status)
   label.textContent = status.replace('-', ' ')
 }
