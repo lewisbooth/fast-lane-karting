633447f618054c92311b58edc57f010ddd14a5fa
Lewis Booth <lewis@amp.studio>
2020-12-16 09:53:08 +0000

    Removed Covid page

--- src/js/openingHours.js
+++ src/js/openingHours.js
@@ -6,21 +6,22 @@ updateLight()
 setTimeout(updateLight, 5000)
 
 function updateLight() {
-  // const date = new Date()
-  // const day = date.getDay()
-  // const hour = date.getHours()
-  // const minutes = date.getMinutes()
-  // if (hour >= 10 && hour < 22)
-  //   changeStatus('open-now')
-  // else if (day == 6 && hour >= 9 && hour < 22)
-  //   changeStatus('open-now')
-  // else if (hour == 22 && minutes < 30)
-  //   changeStatus('closing-soon')
-  // else
+  const date = new Date()
+  const day = date.getDay()
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  if (hour >= 9 && hour < 22)
+    changeStatus('open-now')
+  else if (day == 6 && hour >= 9 && hour < 22)
+    changeStatus('open-now')
+  else if (hour == 22 && minutes < 30)
+    changeStatus('closing-soon')
+  else
     changeStatus('closed')
 }
 
 function changeStatus(status) {
   light.classList.add(status)
   label.textContent = status.replace('-', ' ')
+  console.log(status);
 } 
